03d0c0e965df38d7858916919ab48ac7e6ed8758
Antonia Atanasova <antoniyaata@gmail.com>
2021-03-20 12:01:47 +0200

    Added get class name util

---
+++ businesses-board/src/scripts/utils.ts
@@ -0,0 +1,6 @@
+const getClassName = (classNamesMap: { [key: string]: boolean }) => Object.keys(classNamesMap)
+  .filter((className) => classNamesMap[className])
+  .join(' ')
+  .trim();
+
+export { getClassName };
