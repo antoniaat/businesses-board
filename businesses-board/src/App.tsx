72a9ab0649855a88fa757b25717313723dbd9bb6
Antonia Atanasova <antoniyaata@gmail.com>
2021-03-20 10:32:55 +0200

    Added navigation component and styles

--- businesses-board/src/App.tsx
+++ businesses-board/src/App.tsx
@@ -7,14 +7,18 @@ import {
 } from "react-router-dom";
 import Business from './scripts/components/business';
 import Board from './scripts/components/board';
+import Navigation from './scripts/components/navigation';
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Board} />
-      <Route path="/businesses/:id" component={Business} />
-    </Switch>
-  </Router>
+  <>
+    <Navigation />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Board} />
+        <Route path="/businesses/:id" component={Business} />
+      </Switch>
+    </Router>
+  </>
 );
 
 export default App;
---
+++ businesses-board/src/scripts/components/navigation/index.tsx
@@ -0,0 +1,11 @@
+import logo from '../../../assets/images/logo.png';
+
+import '../../../styles/components/navigation/navigation.scss';
+
+const Navigation = () => (
+    <nav className="navigation">
+        <img src={logo} alt="Logo" />
+    </nav>
+);
+
+export default Navigation;
