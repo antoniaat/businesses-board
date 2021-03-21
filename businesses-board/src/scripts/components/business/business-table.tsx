888e9026a96e87b82be597dbc8ff03b13fdf57b6
Antonia Atanasova <antoniyaata@gmail.com>
2021-03-21 16:39:55 +0200

    Improved the table loading indicators

--- businesses-board/src/scripts/components/business/business-table.tsx
+++ businesses-board/src/scripts/components/business/business-table.tsx
@@ -1,39 +1,59 @@
 import '../../../styles/components/business/business-table.scss';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { BusinessTableProps } from '../../types/business-table';
 import { defaultBusinessData } from '../../utils/constants';
+import ContentLoading from '../content-loading';
 
-const BusinessTable: React.FC<BusinessTableProps> = ({ isLoading, data = defaultBusinessData }) => {
-  console.log(isLoading, 'isLoading');
-  console.log(data, 'data');
+const BusinessTable: React.FC<BusinessTableProps> = (
+  {
+    isLoading = true,
+    data = defaultBusinessData,
+  },
+) => {
+  const history = useHistory();
+  const [businesses, setBusinesses] = useState(defaultBusinessData);
+
+  useEffect(() => {
+    setBusinesses(data);
+  }, [isLoading]);
+
+  const handleOnClick = (event: any) => {
+    const element = event.target as HTMLElement;
+    const id = (element.parentElement?.id as string);
+    const businessLink = `/business/${id}`;
+
+    history.push(businessLink);
+  };
 
   return (
     <table className="business-table">
       <thead className="business-table-header">
         <tr className="business-table-row">
           <td className="business-table-header-cell business-table-cell">
-            name
-            {/* <ContentLoading text="Name" isLoading={isLoading} /> */}
+            <ContentLoading text="Name" isLoading={isLoading} />
           </td>
           <td className="business-table-header-cell business-table-cell">
-            desc
-            {/* <ContentLoading text="Description" isLoading={isLoading} /> */}
+            <ContentLoading text="Description" isLoading={isLoading} />
           </td>
         </tr>
       </thead>
       <tbody className="business-table-content">
         {
-          data.map(({ id, name, description }) => (
-            <tr className="business-table-row" key={id}>
+          businesses.map(({ id, name, description }) => (
+            <tr
+              id={id}
+              key={id}
+              className="business-table-row"
+              onClick={handleOnClick}
+            >
               <td className="business-table-cell">
-                {name}
-                {/* <ContentLoading text={name} isLoading={isLoading} /> */}
+                <ContentLoading text={name} isLoading={isLoading} />
               </td>
               <td className="business-table-cell">
-                {description}
-                {/* <ContentLoading text={description} isLoading={isLoading} /> */}
+                <ContentLoading text={description} isLoading={isLoading} />
               </td>
             </tr>
           ))
@@ -43,6 +63,6 @@ const BusinessTable: React.FC<BusinessTableProps> = ({ isLoading, data = default
   );
 };
 
-const mapStateToProps = (state: any) => state;
+const mapStateToProps = (state: Object) => state;
 
 export default connect(mapStateToProps)(BusinessTable);
