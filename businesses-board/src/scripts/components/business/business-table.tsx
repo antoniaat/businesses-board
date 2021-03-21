867d9e866823a284f274f7d43ebf1e6702632caa
Antonia Atanasova <antoniyaata@gmail.com>
2021-03-21 14:34:58 +0200

    Fixed the business table data to come from redux not from props

--- businesses-board/src/scripts/components/business/business-table.tsx
+++ businesses-board/src/scripts/components/business/business-table.tsx
@@ -1,31 +1,48 @@
 import '../../../styles/components/business/business-table.scss';
 
 import React from 'react';
+import { connect } from 'react-redux';
 import { BusinessTableProps } from '../../types/business-table';
+import { defaultBusinessData } from '../../utils/constants';
 
-const BusinessTable: React.FC<BusinessTableProps> = ({ business }) => (
-  <table className="business-table">
-    <thead className="business-table-header">
-      <tr className="business-table-row">
-        <td className="business-table-header-cell business-table-cell">Name</td>
-        <td className="business-table-header-cell business-table-cell">Description</td>
-      </tr>
-    </thead>
-    <tbody className="business-table-content">
-      {
-        business.map(({ id, name, description }) => (
-          <tr className="business-table-row" key={id}>
-            <td className="business-table-cell">
-              {name}
-            </td>
-            <td className="business-table-cell">
-              {description}
-            </td>
-          </tr>
-        ))
-      }
-    </tbody>
-  </table>
-);
+const BusinessTable: React.FC<BusinessTableProps> = ({ isLoading, data = defaultBusinessData }) => {
+  console.log(isLoading, 'isLoading');
+  console.log(data, 'data');
 
-export default BusinessTable;
+  return (
+    <table className="business-table">
+      <thead className="business-table-header">
+        <tr className="business-table-row">
+          <td className="business-table-header-cell business-table-cell">
+            name
+            {/* <ContentLoading text="Name" isLoading={isLoading} /> */}
+          </td>
+          <td className="business-table-header-cell business-table-cell">
+            desc
+            {/* <ContentLoading text="Description" isLoading={isLoading} /> */}
+          </td>
+        </tr>
+      </thead>
+      <tbody className="business-table-content">
+        {
+          data.map(({ id, name, description }) => (
+            <tr className="business-table-row" key={id}>
+              <td className="business-table-cell">
+                {name}
+                {/* <ContentLoading text={name} isLoading={isLoading} /> */}
+              </td>
+              <td className="business-table-cell">
+                {description}
+                {/* <ContentLoading text={description} isLoading={isLoading} /> */}
+              </td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  );
+};
+
+const mapStateToProps = (state: any) => state;
+
+export default connect(mapStateToProps)(BusinessTable);
